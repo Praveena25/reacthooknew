760383917665b218b5cf4423f0d1e6b35a5a5a73
Your Name <praveenagovind25@gmail.com>
2021-11-18 20:18:59 -0800

    pass multiple context objects between components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import './App.css';
 import ComponentA from './components/contexthooks/ComponentA';
-import DataFetching from './components/hooks/DataFetching';
 export const UserContext = React.createContext()
+export const BatchContext = React.createContext()
 
 function App() {
   return (
     <div className="App">
-      <UserContext.Provider value = {'Praveena'} >
-    <ComponentA/>
-     </UserContext.Provider>
+      <UserContext.Provider value={'Praveena'} >
+        <BatchContext.Provider value={'dxc batch 2'}>
+        <ComponentA/>
+     
+    </BatchContext.Provider>
+    </UserContext.Provider>
       
-    </div>
+    </div >
   );
 }
 
