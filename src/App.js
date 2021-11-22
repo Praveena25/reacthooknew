11888b2e093669d9a69ee24896baf12e0ddb9f87
Your Name <praveenagovind25@gmail.com>
2021-11-22 03:26:13 -0800

    data fetching useReducer hook

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { useReducer } from 'react';
+import CounterTwo from './components/reducerhook/CounterTwo';
+import CounterThree from './components/reducerhook/CounterThree';
+import CompA from './components/contextNreducer/CompA';
+import CompB from './components/contextNreducer/CompB';
+import CompC from './components/contextNreducer/CompC';
 import DataFectching from './components/useReducercases/DataFectching';
+import DataFetchingTwo from './components/useReducercases/DataFetchingTwo';
 
 export const CountContext = React.createContext()
 const initialState = 0
+
 const reducer = (state, action) => {
   switch (action) {
     case 'increment': return state + 1
@@ -15,13 +22,13 @@ const reducer = (state, action) => {
 }
 
 function App() {
+  return (
 
-return (
-  
-  <div className="App">
-    <DataFectching />
+    <div className="App">
+      <DataFetchingTwo />
     </div>
-    
+
   );
- }
- export default App
+}
+
+export default App;
